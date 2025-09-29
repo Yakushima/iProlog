09667ee3118436ad8b9314bb62b206a75359a070
Michael Turner <michael.eugene.turner@gmail.com>
2025-09-29 14:15:36 +0900

    negation hack seems to work

---
+++ IP/progs/neg_succ.pl
@@ -0,0 +1,14 @@
+something(b).
+bad(a).
+not(something(c)).
+% not(something(a)).
+
+goal(P) :-
+  bad(P),
+  not(something(P)).
+
+% Problem:
+%	P <- a
+%       not _0
+%	_0 to hold something(P)
+%		fails with P=a
